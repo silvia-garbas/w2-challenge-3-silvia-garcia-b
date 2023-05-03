6162aa47efd64cc19c20d87a44c5def108ad09e3
Silvia <silvia.garbas@gmail.com>
2023-05-03 18:29:49 +0200

    Add main.js & style.css & index.html

---
+++ main.js
@@ -0,0 +1,30 @@
+const counter = (value) => {
+    const incrementButton = document.querySelector(".increment-button");
+    incrementButton.addEventListener("click", () => {
+        let result = Number(value.textContent) + 1;
+        if (result > 20) {
+            return;
+        }
+        value.textContent = result;
+    });
+    const decrementButton = document.querySelector(".decrement-button");
+    decrementButton.addEventListener("click", () => {
+        let result = Number(value.textContent) - 1;
+        if (result < 0) { 
+            result = 0;
+        }
+        value.textContent = result;
+    });
+    const restartButton = document.querySelector(".restart-button");
+    restartButton.addEventListener("click", () => {
+        let result = 0;
+        value.textContent = result;
+    });
+};
+const playCounter = () => {
+    let value = document.querySelector(".counter-number");
+    let result;
+    counter(value);
+};
+
+playCounter();
